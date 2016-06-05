d8558c2c4ed87a849848e58adc52f66ae9429423
brytree <brycebortree@gmail.com>
2016-06-05 13:01:45 -0700

    fixed controller syntax

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,9 +1,9 @@
 angular.module('WriteCtrls', ['WriteServices'])
 
-.controller('WriteCtrls', '$scope', '$state', function($scope, $state){
+.controller('WriteCtrls', ['$scope', '$state', function($scope, $state){
   $scope.in = "yes";
-})
+}])
 
-.controller('HomeCtrl', '$scope', '$state', function($scope, $state){
+.controller('HomeCtrl', ['$scope', '$state', function($scope, $state){
   $scope.in = "yes";
-})
+}])
