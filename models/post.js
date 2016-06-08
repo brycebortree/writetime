1ccefe7e55520215ad50e42e56baa13c00aea24f
brytree <brycebortree@gmail.com>
2016-06-08 10:48:51 -0700

    created comments model

--- models/post.js
+++ models/post.js
@@ -8,6 +8,7 @@ module.exports = function(sequelize, DataTypes) {
     classMethods: {
       associate: function(models) {
         models.post.belongsTo(models.user);
+        models.post.hasMany(models.comment);
       }
     }
   });
